62027d964b9f00c2721c44d5cf53a50bb27b00e3
jayy1511 <jay1511dj@gmail.com>
2025-01-17 20:26:46 +0100

    updated

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,16 +2,13 @@ const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator')
 
-
 module.exports.registerUser = async (req, res, next ) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors : errors.array() });
     }
-
-    console.log(req.body);
-
+    
     const { fullname, email, password } = req.body;
 
     const hashedPassword = await userModel.hashPassword(password);
