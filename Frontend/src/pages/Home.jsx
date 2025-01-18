d027423ab358ddc0029f5f30d75aa43db19a065c
jayy1511 <jay1511dj@gmail.com>
2025-01-18 13:44:59 +0100

    frontend setup done

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../assets/1 (2).png'
+import background from '../assets/3.webp'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='bg-cover bg-center bg-[url(https://i.ibb.co/SQY9j0M/3.webp)] h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+      <img className='w-28 ml-8' src={logo} alt="Voyager Logo" />
+        <div className='bg-white pb-7 py-4 px-4'>
+            <h2 className='text-3xl font-bold'>Get Started with Voyager</h2>
+            <button className='w-full bg-black text-white py-3 rounded mt-5'>Continue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
