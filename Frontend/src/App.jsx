03b22edf0b39ba36cde83eea466a70d375329df8
jayy1511 <jay1511dj@gmail.com>
2025-03-25 14:19:34 +0100

    CaptainProtectWrapper Created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ import UserSignup from './pages/UserSignUp'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
 import Start from './pages/Start'
-import UserProtectedWrapper from './pages/UserProtectedWrapper'
+import UserProtectWrapper from './pages/UserProtectWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
@@ -25,9 +25,9 @@ const App = () => {
             <Home />
           </UserProtectedWrapper>
         } />
-        <Route path='/user/logout' element={ <UserProtectedWrapper>
+        <Route path='/user/logout' element={ <UserProtectWrapper>
           <UserLogout />
-        </UserProtectedWrapper>
+        </UserProtectWrapper>
         } />
         <Route path='/captain-home' element={
           <CaptainProtectWrapper>
